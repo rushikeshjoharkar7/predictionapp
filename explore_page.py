78873a8ca19989df466ee3fa13a8110a9ed1b636
Rushikesh Joharkar <88484461+rushikeshjoharkar7@users.noreply.github.com>
2023-02-10 16:45:58 +0000

    Add files via upload

---
+++ explore_page.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+
+    st.write(
+        """
+    ### Stack Overflow Developer Survey 2020
+    """
+    )
+
+
---
+++ predict_page.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+def load_model():
+    with open('model.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regression = data["model"]
+
+
+
+def show_predict_page():
+    st.title("Yarn count Prediction from Fibre Properties")
+
+    st.write("""### We need some information to predict the Yarn Count (tex)""")
+
+   
+    A = st.number_input(label="2.5% span length (mm)",step=1.,format="%.2f")
+    B = st.number_input(label="UR (%)",step=1.,format="%.2f")
+    C = st.number_input(label="Fineness (ug/inch)",step=1.,format="%.2f")
+    D = st.number_input(label="Bundle Strength (cN/tex)",step=1.,format="%.2f")
+    E = st.number_input(label="Trash content (%)",step=1.,format="%.2f")
+
+
+    ok = st.button("Calculate Yarn Count (tex)")
+    
+    if ok:
+        x = np.array([[A,B,C,D,E]])
+        
+       
+        Count = regression.predict(x)
+        
+        st.subheader(f"The estimated count is {Count[0]:.8f}")
